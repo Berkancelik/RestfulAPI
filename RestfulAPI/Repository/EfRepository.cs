64d298e466533dcf85e54db4c44b59526bfb5d30
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-02-01 23:40:13 +0300

    RepositoryPattern applied adn EfRepository completed

---
+++ RestfulAPI/Repository/EfRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulAPI.DataContext;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestfulAPI.Repository
+{
+    public class EfRepository<T> : IGenericRepository<T> where T : class, new()
+    {
+        private readonly Context _context;
+        private readonly DbSet<T> _dbSet;
+
+        public EfRepository(Context context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = dbSet;
+        }
+
+        public T Add(T entity)
+        {
+            var result = new T();
+            _dbSet.Add(entity); 
+            _context.SaveChanges();
+            result = entity;
+            return result;
+         }
+
+        public T Delete(T entity)
+        {
+            var result = new T();
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+            result = entity;
+            return result;
+        }
+
+        public List<T> GetAll()
+        {
+            var result = new List<T>();
+            var list = _dbSet.ToList();
+            if (list != null)
+                result = list;
+            else            
+                result = null;
+                return result;
+            
+          
+         }
+
+        public T GetById(int id)
+        {
+            var result = new T();
+            result = _dbSet.Find(id);
+            return result;
+         }
+
+        public T UpdateById(T entity, int id)
+        {
+            var result = new T();
+            var model = _dbSet.Find(id);
+
+            if (model == null)
+                return null;
+            var response = _context.Entry(model);
+            response.State = EntityState.Modified;
+            _context.SaveChanges();
+            result = response.Entity;
+            return result;
+        }
+    }
+}
---
+++ RestfulAPI/Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestfulAPI.Repository
+{
+    public interface IGenericRepository<T> where T : class, new()
+    {
+        T Add(T entity);
+        T Delete(T entity);
+        T GetById(int id);
+        List<T> GetAll();
+        T UpdateById(T entity, int id);
+    }
+}
